53735eb8f66cd1750325a16849e31fae444563ee
MaXinPeng <1538139147@qq.com>
2022-04-04 17:28:49 +0800

    Updates

---
+++ precache-manifest.8a5e89e18291941272ac023c61efa00b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e1c78d09c47ce14d021",
+    "url": "/mall-comment/static/css/main.881c875e.chunk.css"
+  },
+  {
+    "revision": "7e1c78d09c47ce14d021",
+    "url": "/mall-comment/static/js/main.7e1c78d0.chunk.js"
+  },
+  {
+    "revision": "6ee94f985ede9c95170e",
+    "url": "/mall-comment/static/js/1.6ee94f98.chunk.js"
+  },
+  {
+    "revision": "15441339e72e18070a65",
+    "url": "/mall-comment/static/js/runtime~main.15441339.js"
+  },
+  {
+    "revision": "ece34b457baf7f87f2d83cebb6b8957d",
+    "url": "/mall-comment/index.html"
+  }
+];
